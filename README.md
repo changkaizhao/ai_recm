256f9fa488c066d733244593f465b6c27d6cd336
Roby <8423225+changkaizhao@users.noreply.github.com>
2023-03-09 02:46:35 +0000

    update project setting

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# ai_recm
+# Introduction
+
+## 1.  Config data files
+You can put all json data files in somewhere and mounting them to docker volume, but recommend putting them in **data** directory, and modify all files path in
+`data/data.json`, indicates each datafilepath to the specific filepath which file located.
+
+## 2. Log file 
+You can find **log** file in `log` directory which located in `workspacedir`.
+
+
+
+## 3. Sending Test message
+
+You can use `http` `POST` method to send message to Solace queue.
+·
+>API:  10.194.102.62:8080/SolMessager/msg
+
+requeset data form as below
+```json
+{
+  "bet_acc_no": "3540498",
+  "request_id": "REQUESTID_PLACEHOLDER",
+  "meeting_id": "20210716",
+  "betslip_display_num": 2,
+  "betconfirmation_display_num": 3,
+   "bettype_display_num":{
+    "WIN_PLA_W-P":2,
+    "QIN":2,
+    "QPL":2,
+    "QQP":2,
+    "FCT":2,
+    "TCE":2,
+    "TRI":2,
+    "FF":2,
+    "QTT":2,
+    "CWA":2
+   },
+  "betfilter": [
+     {
+        "meeting_id": "20210716",
+        "race_id": "RACE_20210314_0001",
+        "venue": "ST",
+        "race_no": "1",
+        "pool": {
+          "pool_type": "WIN",
+          "race_no": [
+            "1"
+          ],
+          "sel": [
+            "6"
+          ]
+        }
+     },
+     {
+        "meeting_id": "20210716",
+        "race_id": "RACE_20210314_0008",
+        "venue": "ST",
+        "race_no": "3",
+        "pool": {
+          "pool_type": "QPL",
+          "race_no": [
+            "3"
+          ],
+          "sel": [
+            "3",
+            "4"
+          ]
+        }
+     }
+  ],
+  "race_no":7,
+  "start_time": "2021-06-09T14:30:00+08:00"
+}
+```
+
+
+[INFO ] 2022-10-17 17:02:10,683(237003) --> [Context_2_jms_1_ConsumerDispatcher] com.HKJC.Main$1.onMessage(Main.java:188
+): Properties: JMS_Solace_DeliverToOneJMS_Solace_DeadMsgQueueEligibleJMS_Solace_ElidingEligibleSolace_JMS_Prop_IS_Reply_
+Message
+
--- src/main/java/com/HKJC/Config/Configurator.java
+++ src/main/java/com/HKJC/Config/Configurator.java
@@ -6,7 +6,6 @@ import org.apache.log4j.Logger;
 
 public class Configurator {
     // some constants
-    private boolean inited = false;
     public final boolean Debug = false;
     public final DataProviderType dataProviderType = DataProviderType.FileData;
 
@@ -18,34 +17,31 @@ public class Configurator {
     public final String CONNECTION_FACTORY_JNDI_NAME = "apConnectionFactory";
     public String host;
     public String username;
-    public  String password;
+    public String password;
     public final String vpnName = "intg";
 
-    public int CPUNum = 32;//Runtime.getRuntime().availableProcessors();
-    public  boolean useKerberosAuth = true;
+    public int CPUNum = 32;// Runtime.getRuntime().availableProcessors();
+    public boolean useKerberosAuth = true;
 
-    private  String fixture_endpoint;
-    private  String mapr_endpoint ;
-
+    private String fixture_endpoint;
+    private String mapr_endpoint;
 
     public Boolean SSL_VALIDATE_CERTIFICATE = false;
     public String SSL_TRUST_STORE = "";
     public String SSL_TRUST_STORE_FORMAT = "";
     public String SSL_TRUST_STORE_PASSWORD = "";
-    
+
     public String dataIP = "";
 
     public final String api_racetime = "/api/racetime";
-   public final String api_winodds = "/api/winodds";
-   public final String api_cwagroup = "/api/cwagroup";
-   public final String api_racepool = "/api/raceandpool";
-   public final String api_runner = "/api/runner";
-
+    public final String api_winodds = "/api/winodds";
+    public final String api_cwagroup = "/api/cwagroup";
+    public final String api_racepool = "/api/raceandpool";
+    public final String api_runner = "/api/runner";
 
     public final String api_horse = "/horse";
     public final String api_bettype = "/bettype";
     public final String api_raceProximity = "/race_proximity";
-
 
     public final String tbl_path_bettype = "/exthcp/airecw/rs/table/bettype-selection-precompute";
     public final String tbl_path_horse = "/exthcp/airecw/rs/table/horse-selection-precompute";
@@ -59,77 +55,160 @@ public class Configurator {
     // singleton
     private static Configurator instance = null;
 
-    public String genAPI(String api){
+    public String genAPI(String api) {
         return this.mapr_endpoint + api;
     }
 
-    public String genAPI(String api, String meeting_id){
-        return this.fixture_endpoint + api + "?meeting_id="+meeting_id;
+    public String genAPI(String api, String meeting_id) {
+        return this.fixture_endpoint + api + "?meeting_id=" + meeting_id;
     }
 
-    public String genAPI(String api, String meeting_id, String acct_id){
-        return this.mapr_endpoint + api + "/"+meeting_id+"/"+acct_id;
+    public String genAPI(String api, String meeting_id, String acct_id) {
+        return this.mapr_endpoint + api + "/" + meeting_id + "/" + acct_id;
     }
-    private String getMaprConnectionString() throws Exception{
+
+    private String getMaprConnectionString() throws Exception {
         String hostname = "";
         String port = "";
         String user = "";
         String password = "mapr";
         String certPath = "";
         String nodeFQDN = "";
-
-        try{ hostname = System.getenv("MAPRDB_HOSTNAME");}catch (Exception e){ throw new Error("MAPRDB_HOSTNAME env var is not defined");}
-        try{ port = System.getenv("MAPRDB_PORT");}catch (Exception e){throw new Error("MAPRDB_PORT env var is not defined");}
-        try{  user = System.getenv("MAPRDB_USER");}catch (Exception e){throw new Error("MAPRDB_USER env var is not defined");}
-        try{  certPath = System.getenv("MAPRDB_CERT_PATH");}catch (Exception e){throw new Error("MAPRDB_CERT_PATH env var is not defined");}
-        try{  nodeFQDN = System.getenv("MAPRDB_NODE_FQDN");}catch (Exception e){throw new Error("MAPRDB_NODE_FQDN env var is not defined");}
-        return "ojai:mapr:@" + hostname + ":" + port + "?auth=basic;user=" + user + ";password=" + password + ";ssl=true;sslCA=" + certPath + ";sslTargetNameOverride=" + nodeFQDN;
-
-    }
-    public void init() throws Exception{
-        if(this.Debug){ return; }
         Logger logger = Logger.getLogger(Configurator.class);
 
-        try{this.host = System.getenv("CAP_SOLACE");}catch (Exception e){logger.error(new Error("CAP_SOLACE env var is not defined")); throw e;}
-        try{this.username = System.getenv("SOL_USR");}catch (Exception e){logger.error(new Error("SOL_USR env var is not defined"));throw e;}
-        try{this.password = System.getenv("SOL_PWD");}catch (Exception e){logger.error(new Error("SOL_PWD env var is not defined"));throw e;}
-        try{this.fixture_endpoint = System.getenv("FIXTURE_ENDPOINT");}catch (Exception e){logger.error(new Error("FIXTURE_ENDPOINT env var is not defined"));throw e;}
-//        try{instance.mapr_endpoint = System.getenv("MAPR_ENDPOINT");}catch (Exception e){logger.error(new Error("MAPR_ENDPOINT env var is not defined"));}
-
-        try{
-            this.SSL_VALIDATE_CERTIFICATE = Boolean.parseBoolean(System.getenv("SSL_VALIDATE_CERTIFICATE"));
-        }catch (Exception e){
-            this.SSL_VALIDATE_CERTIFICATE = false;
-            logger.warn("SSL_VALIDATE_CERTIFICATE env var is not defined, use default false.");
+        hostname = System.getenv("MAPRDB_HOSTNAME");
+        if (hostname == null) {
+            Error e = new Error("MAPRDB_HOSTNAME env var is not defined");
             throw e;
         }
-        try{this.SSL_TRUST_STORE = System.getenv("SSL_TRUST_STORE");}catch (Exception e){logger.error(new Error("SSL_TRUST_STORE env var is not defined"));throw e;}
-        try{this.SSL_TRUST_STORE_FORMAT = System.getenv("SSL_TRUST_STORE_FORMAT");}catch (Exception e){logger.error(new Error("SSL_TRUST_STORE_FORMAT env var is not defined"));throw e;}
-        try{this.SSL_TRUST_STORE_PASSWORD = System.getenv("SSL_TRUST_STORE_PASSWORD");}catch (Exception e){logger.error(new Error("SSL_TRUST_STORE_PASSWORD env var is not defined"));throw e;}
 
-        try{this.fileRootPath = System.getenv("FILE_ROOT_PATH");}catch (Exception e){logger.error(new Error("FILE_ROOT_PATH env var is not defined"));throw e;}
-
-        try{
-            this.useKerberosAuth = Boolean.parseBoolean(System.getenv("USE_KERBEROS"));
-        }catch (Exception e){
-            this.useKerberosAuth = false;
-            logger.warn("USE_KERBEROS env var is not defined, use default false.");
+        port = System.getenv("MAPRDB_PORT");
+        if (port == null) {
+            Error e = new Error("MAPRDB_PORT env var is not defined");
+            throw e;
         }
 
-        try{
+        user = System.getenv("MAPRDB_USER");
+        if (user == null) {
+            Error e = new Error("MAPRDB_USER env var is not defined");
+            throw e;
+        }
+
+        certPath = System.getenv("MAPRDB_CERT_PATH");
+        if (certPath == null) {
+            Error e = new Error("MAPRDB_CERT_PATH env var is not defined");
+            throw e;
+        }
+
+        nodeFQDN = System.getenv("MAPRDB_NODE_FQDN");
+        if (nodeFQDN == null) {
+            Error e = new Error("MAPRDB_NODE_FQDN env var is not defined");
+            throw e;
+        }
+
+        return "ojai:mapr:@" + hostname + ":" + port + "?auth=basic;user=" + user + ";password=" + password
+                + ";ssl=true;sslCA=" + certPath + ";sslTargetNameOverride=" + nodeFQDN;
+
+    }
+
+    public void init() throws Exception {
+        if (this.Debug) {
+            return;
+        }
+        Logger logger = Logger.getLogger(Configurator.class);
+
+        try {
             this.CPUNum = Integer.parseInt(System.getenv("THREAD_SIZE"));
-        }catch (Exception e){
+        } catch (Exception e) {
             this.CPUNum = 2;
             logger.warn("THREAD_SIZE env var is not defined, use default 2");
         }
 
-        try{this.mapr_connection = this.getMaprConnectionString();}catch (Exception e){
+        try {
+            this.useKerberosAuth = Boolean.parseBoolean(System.getenv("USE_KERBEROS"));
+        } catch (Exception e) {
+            this.useKerberosAuth = false;
+            logger.warn("USE_KERBEROS env var is not defined, use default false.");
+        }
+
+        this.host = System.getenv("CAP_SOLACE");
+        if (this.host == null) {
+            Error e = new Error("CAP_SOLACE env var is not defined");
             logger.error(e);
             throw e;
         }
 
-        this.inited = true;
+        this.username = System.getenv("SOL_USR");
+        if (this.username == null) {
+            Error e = new Error("SOL_USR env var is not defined");
+            logger.error(e);
+            throw e;
+        }
+
+        this.password = System.getenv("SOL_PWD");
+        if (this.password == null) {
+            Error e = new Error("SOL_PWD env var is not defined");
+            logger.error(e);
+            throw e;
+        }
+
+        this.fixture_endpoint = System.getenv("FIXTURE_ENDPOINT");
+        if (this.fixture_endpoint == null) {
+            Error e = new Error("FIXTURE_ENDPOINT env var is not defined");
+            logger.error(e);
+            throw e;
+        }
+
+        // try{instance.mapr_endpoint = System.getenv("MAPR_ENDPOINT");}catch (Exception
+        // e){logger.error(new Error("MAPR_ENDPOINT env var is not defined"));}
+
+        try {
+            this.SSL_VALIDATE_CERTIFICATE = Boolean.parseBoolean(System.getenv("SSL_VALIDATE_CERTIFICATE"));
+            if (this.SSL_VALIDATE_CERTIFICATE == null) {
+                this.SSL_VALIDATE_CERTIFICATE = false;
+                logger.warn("SSL_VALIDATE_CERTIFICATE env var is not defined, use default false");
+            }
+        } catch (Exception e) {
+            this.SSL_VALIDATE_CERTIFICATE = false;
+            logger.warn("SSL_VALIDATE_CERTIFICATE env var is not defined, use default false.");
+        }
+        this.SSL_TRUST_STORE = System.getenv("SSL_TRUST_STORE");
+        if (this.SSL_TRUST_STORE == null) {
+            Error e = new Error("SSL_TRUST_STORE env var is not defined");
+            logger.error(e);
+            throw e;
+        }
+
+        this.SSL_TRUST_STORE_FORMAT = System.getenv("SSL_TRUST_STORE_FORMAT");
+        if (this.SSL_TRUST_STORE_FORMAT == null) {
+            Error e = new Error("SSL_TRUST_STORE_FORMAT env var is not defined");
+            logger.error(e);
+            throw e;
+        }
+
+        this.SSL_TRUST_STORE_PASSWORD = System.getenv("SSL_TRUST_STORE_PASSWORD");
+        if (this.SSL_TRUST_STORE_PASSWORD == null) {
+            Error e = new Error("SSL_TRUST_STORE_PASSWORD env var is not defined");
+            logger.error(e);
+            throw e;
+        }
+
+        this.fileRootPath = System.getenv("FILE_ROOT_PATH");
+        if (this.fileRootPath == null) {
+            Error e = new Error("FILE_ROOT_PATH env var is not defined");
+            logger.error(e);
+            throw e;
+        }
+
+        try {
+            this.mapr_connection = this.getMaprConnectionString();
+        } catch (Exception e) {
+            logger.error(e);
+            throw e;
+        }
+
     }
+
     public static Configurator getInstance() {
         if (instance == null) {
             instance = new Configurator();
